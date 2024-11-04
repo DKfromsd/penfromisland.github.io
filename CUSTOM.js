79f4c6a1b2ecd8669f3efa38f6b37b62a0b87965
DoKyu Lee <dokyulee@mac.lan>
2024-11-04 00:52:38 -0500

    init-file

---
+++ CUSTOM.js
@@ -0,0 +1,25 @@
+"use strict"
+
+/*
+ * Anthony note: Changing these texts may cause severe side effects. 
+ * Please beware of changing the length of texts and number of words. 
+ */
+const phrase1 = `Illustrations`;
+// const phrase2 = `Add your colors on your kid's future`;
+const phrase3 = `Childrens Book`;
+const phrase4 = `Bring attentions`;
+const phrase5 = `Nurture your kids`;
+const phrase6 = `Draw futures with their dreams`;
+
+const email = `info@rysonconsulting.com`;
+const phone = `(123) 456-7890`;
+
+const messageSuccess = `Your request is successfully sent.`;
+const messageFail = `Sorry my mailbox is not set up yet. Please email us at info@rysonconsulting.com`;
+
+const mainDesc = `
+    We offer a personalized Web Starter Kit at a minimal cost, providing everything you need to 
+    jumpstart your website. With our service, you can present your business online with a custom domain 
+    in an ad-free environment within a couple of hours.
+`;
+
